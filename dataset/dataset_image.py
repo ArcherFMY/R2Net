c4c75a586072cb9b4edde3334b122422c361ed2f
archerfmy <archerfmy@gmail.com>
2020-08-06 17:36:37 +0800

    added joint transform

--- dataset/dataset_image.py
+++ dataset/dataset_image.py
@@ -9,6 +9,7 @@ from torchvision.transforms import functional as F
 import torch.nn.functional as Fnn
 import numpy as np
 import random
+from .joint_transforms import Compose, JointResize, RandomHorizontallyFlip, RandomRotate
 
 def load_batch(config, sal_root, im_name, gt_name, is_randflip_h, is_randflip_v):
     img = Image.open(os.path.join(sal_root, im_name)).convert('RGB')
@@ -39,6 +40,35 @@ def load_batch(config, sal_root, im_name, gt_name, is_randflip_h, is_randflip_v)
  
     return img, labels
 
+def load_batch_joint_trans(config, sal_root, im_name, gt_name, joint_trans, img_trans, mask_trans):
+    label = Image.open(sal_root + gt_name).convert('L')
+    img = Image.open(sal_root + im_name).convert('RGB')
+    img, label = joint_trans(img, label)
+    img = img_trans(img)
+    label1 = mask_trans(label)
+    label1 = label1.ge(0.5).float()
+
+    label2 = transforms.Resize((config.input_size//2, config.input_size//2))(label)
+    label2 = mask_trans(label2)
+    label2 = label2.ge(0.5).float()
+
+    label4 = transforms.Resize((config.input_size//4, config.input_size//4))(label)
+    label4 = mask_trans(label4)
+    label4 = label4.ge(0.5).float()
+
+    label8 = transforms.Resize((config.input_size//8, config.input_size//8))(label)
+    label8 = mask_trans(label8)
+    label8 = label8.ge(0.5).float()
+
+    label16 = transforms.Resize((config.input_size//16, config.input_size//16))(label)
+    label16 = mask_trans(label16)
+    label16 = label16.ge(0.5).float()
+    labels = {'1': label1, '2': label2, '4': label4, '8': label8, '16': label16}
+ 
+    return img, labels
+
+
+
 def load_batch_test(config, data_root, im_name):
     img = Image.open(os.path.join(data_root, im_name)).convert('RGB')
     im_size = img.size
@@ -57,7 +87,17 @@ class ImageDataTrain(data.Dataset):
             self.sal_list = [x.strip() for x in f.readlines()]
 
         self.sal_num = len(self.sal_list)
-
+        self.joint_transform = Compose([JointResize(self.config.input_size), RandomHorizontallyFlip(), RandomRotate(10)])
+        img_transform = [transforms.ColorJitter(0.1, 0.1, 0.1)]
+        self.img_transform = transforms.Compose(
+            [
+                *img_transform, 
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]
+        )
+        self.mask_transform = transforms.ToTensor()
+ 
     def __getitem__(self, item):
         # sal data loading
         im_root = self.sal_list[item % self.sal_num].split()[0]
@@ -67,22 +107,23 @@ class ImageDataTrain(data.Dataset):
         im_name = os.path.join(im_root, currt_img + '.jpg') 
         gt_name = os.path.join(gt_root, currt_img + '.png')
 
-        if self.config.do_aug:
-            if self.config.do_h_flip:
-                is_randflip_h = random.random()
-            else:
-                is_randflip_h = 0
+        #if self.config.do_aug:
+        #    if self.config.do_h_flip:
+        #        is_randflip_h = random.random()
+        #    else:
+        #        is_randflip_h = 0
+        #
+        #    if self.config.do_v_flip:
+        #        is_randflip_v = random.random()
+        #
+        #    else:
+        #        is_randflip_v = 0
+        #else:
+        #    is_randflip_h = 0
+        #    is_randflip_v = 0
 
-            if self.config.do_v_flip:
-                is_randflip_v = random.random()
-
-            else:
-                is_randflip_v = 0
-        else:
-            is_randflip_h = 0
-            is_randflip_v = 0
-
-        image, labels = load_batch(self.config, self.sal_root, im_name, gt_name, is_randflip_h, is_randflip_v)  
+        #image, labels = load_batch(self.config, self.sal_root, im_name, gt_name, is_randflip_h, is_randflip_v)  
+        image, labels = load_batch_joint_trans(self.config, self.sal_root, im_name, gt_name, self.joint_transform, self.img_transform, self.mask_transform)  
 
         sample = {'image': image, \
                   'labels': labels}
